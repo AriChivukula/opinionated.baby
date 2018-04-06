5b3ff68e648038e9c7acacb1c36f3db39cbc1f76
Ari Chivukula <ari@chivuku.la>
2018-04-06 11:12:31 -0400

    Let's try deploying remote!

--- src/server/index.remote.js
+++ src/server/index.remote.js
@@ -1,6 +1,7 @@
 // @flow
 
 import 'babel-polyfill';
+import awsServerlessExpress from 'aws-serverless-express';
 import cookieParser from 'cookie-parser'
 import express from 'express';
 import server from './server.js';
@@ -8,4 +9,18 @@ import server from './server.js';
 const app = express();
 app.use(cookieParser());
 app.use('/', server);
-app.listen(parseInt(8080));
+
+const types = [
+  'application/octet-stream',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml'
+]
+
+const serverless = awsServerlessExpress.createServer(app, null, types)
+
+export const handler = (event: Object, context: Object) =>
+  awsServerlessExpress.proxy(serverless, event, context)
