264debd8a8aa058047fca9ad21ef37322ed4c904
Ari Chivukula <ari@chivuku.la>
2018-04-06 12:43:13 -0400

    remove cors

--- src/server/index.remote.js
+++ src/server/index.remote.js
@@ -3,13 +3,12 @@
 import 'babel-polyfill';
 import awsServerlessExpress from 'aws-serverless-express';
 import cookieParser from 'cookie-parser'
-import cors from 'cors';
 import express from 'express';
 import server from './server.js';
 
 const app = express();
 app.use(cookieParser());
-app.use('/', cors(), server);
+app.use('/', server);
 
 const types = [
   'application/octet-stream',
