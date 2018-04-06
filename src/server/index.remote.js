ed821a2b66489f84f3380f7ac74373d37e794d5e
Ari Chivukula <ari@chivuku.la>
2018-04-06 11:58:24 -0400

    Add cors

--- src/server/index.remote.js
+++ src/server/index.remote.js
@@ -3,12 +3,13 @@
 import 'babel-polyfill';
 import awsServerlessExpress from 'aws-serverless-express';
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
 import express from 'express';
 import server from './server.js';
 
 const app = express();
 app.use(cookieParser());
-app.use('/', server);
+app.use('/', cors(), server);
 
 const types = [
   'application/octet-stream',
