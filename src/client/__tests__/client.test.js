3fceb93402850acc9db45801c982661a13fa0c09
Ari Chivukula <ari@chivuku.la>
2018-04-06 22:20:01 -0400

    add client test

--- src/client/__tests__/client.test.js
+++ src/client/__tests__/client.test.js
@@ -1,7 +1,41 @@
 // @flow
 
 import 'babel-polyfill';
+import express from 'express';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {
+  Environment,
+  RecordSource,
+  Store,
+} from 'relay-runtime';
+import {
+  urlMiddleware,
+  RelayNetworkLayer
+} from 'react-relay-network-modern';
 
-test('equals', () => {
-  expect(2).toBe(2);
-});
+import Root from '../views/Root.js';
+import server from '../../server/server.js';
+
+express()
+  .use(server)
+  .listen(8080);
+
+test(
+  'Root',
+  async () => {
+    const environment = new Environment({
+      network: new RelayNetworkLayer([
+        urlMiddleware({
+          url: 'http://127.0.0.1:8080',
+        })
+      ]),
+      store: new Store(new RecordSource())
+    });
+    const component = renderer.create(
+      <Root environment={environment} />,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  }
+);
--- src/server/index.local.js
+++ src/server/index.local.js
@@ -1,12 +1,10 @@
 // @flow
 
 import 'babel-polyfill';
-import cookieParser from 'cookie-parser'
 import express from 'express';
 import server from './server.js';
 
 const app = express();
-app.use(cookieParser());
 app.use('/', express.static('_bin/client'));
 app.use('/graphql', server);
-app.listen(parseInt(8080));
+app.listen(8080);
