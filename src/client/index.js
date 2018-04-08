71d240b07fa6a3f322b721a6f9c36aa791af2492
Ari Chivukula <ari@chivuku.la>
2018-04-07 21:04:30 -0400

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -8,7 +8,7 @@ let api_url = 'http://127.0.0.1:8080/graphql/';
 if (window.location.hostname == 'opinionated.baby') {
   api_url = 'https://api.opinionated.baby/opinionatedbaby/'
 } else if (window.location.hostname == 'beta.opinionated.baby') {
-  api_url = 'https://api.beta.opinionated.baby/opinionatedbaby/'
+  api_url = 'https://api-beta.opinionated.baby/betaopinionatedbaby/'
 }
 
 window.onload = () => client(api_url);
