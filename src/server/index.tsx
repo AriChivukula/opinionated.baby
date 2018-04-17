0814669e7096a7a97bf4d36f95a1e0ca543f7159
Ari Chivukula <ari@chivuku.la>
2018-04-16 20:14:54 -0400

    New try

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -8,31 +8,28 @@ import { server } from "./server";
 const app: express.Express = express();
 app.use(cors());
 
-let lambdaHandler: (event: object, context: object) => void =
-  (event: object, context: object): void => {
-    return;
-  };
-
 if (process.env.ENV === "LAMBDA") {
   app.use("/", server);
-  const serverless: object = lambda.createServer(
-    app,
-    null,
-    [
-      "application/octet-stream",
-      "font/eot",
-      "font/opentype",
-      "font/otf",
-      "image/jpeg",
-      "image/png",
-      "image/svg+xml",
-    ],
+  // tslint:disable-next-line:no-any
+  exports.handler = (event: object, context: object): any => lambda.proxy(
+    lambda.createServer(
+      app,
+      null,
+      [
+        "application/octet-stream",
+        "font/eot",
+        "font/opentype",
+        "font/otf",
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+      ],
+    ),
+    event,
+    context,
   );
-  lambdaHandler = (event: object, context: object): void => lambda.proxy(serverless, event, context);
 } else {
   app.use("/", express.static("_build_3/website"));
   app.use("/graphql", server);
   app.listen(process.env.PORT);
 }
-
-export const handler: (event: object, context: object) => void = lambdaHandler;
