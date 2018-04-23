a2e688089d94ab539cc20bf51c31db7438414150
Ari Chivukula <ari@dds.mil>
2018-04-23 09:48:53 -0400

    Image and content

--- src/website/views/Content.tsx
+++ src/website/views/Content.tsx
@@ -129,6 +129,7 @@ interface IReleases {
 }
 
 const releases: IReleases[] = [
+  { titleA: "Fierce", titleB: "Feline", version: "v5" },
   { titleA: "Staunch", titleB: "Sibling", version: "v4" },
   { titleA: "Tilted", titleB: "Turtle", version: "v3" },
   { titleA: "Assertive", titleB: "Actor", version: "v2" },
