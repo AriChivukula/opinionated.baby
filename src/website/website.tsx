f8f2ab1f8ee5aac6b8d52238393ab3de4e36a9bb
Ari Chivukula <ari@chivuku.la>
2018-04-29 09:32:06 -0400

    util and website

--- src/website/website.tsx
+++ src/website/website.tsx
@@ -1,21 +1,22 @@
+/**
+ * This file is partially generated; only edit bespoke sections.
+ *
+ * SOURCE<<gen/website/website.ts::module>>
+ * BESPOKE<<DEPRECATE>>
+ * SIGNED<<DYce6tUYiX+JearHd0lrwgy8SN3UDyporRwXYF0MAB6KVDC2mRUmYNJhVfijypwaKgUQEEiY9Ulu4JKQeLgikw==>>
+ */
+
 import * as cookie from "js-cookie";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {
-  authMiddleware,
-  RelayNetworkLayer,
-  urlMiddleware,
-} from "react-relay-network-modern";
+import { authMiddleware, RelayNetworkLayer, urlMiddleware } from "react-relay-network-modern";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import {
-  Environment,
-  RecordSource,
-  Store,
-} from "relay-runtime";
+import { Environment, RecordSource, Store } from "relay-runtime";
 
 import { FourOhFour } from "./views/FourOhFour";
 import { Root } from "./views/Root";
 
+/* BESPOKE START <<DEPRECATE>> */
 export function render(apiURL: string): void {
   const environment: Environment = new Environment({
     network: new RelayNetworkLayer([
@@ -47,3 +48,4 @@ export function render(apiURL: string): void {
     document.getElementById("root"),
   );
 }
+/* BESPOKE END <<DEPRECATE>> */
