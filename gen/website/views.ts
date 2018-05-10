7046bdfb89a403ab13e432e4c8285dc5ba08fadb
Ari Chivukula <ari@chivuku.la>
2018-05-09 20:19:33 -0400

    update version

--- gen/website/views.ts
+++ gen/website/views.ts
@@ -1,37 +1,38 @@
 import {
+  ERelayType,
   Module,
   React,
   Type,
 } from "typescriptase";
 
-export const ContentReact: Module = React(
-  "src/website/views/Content.tsx",
-  "Content",
-);
+export const ContentReact: Module = React({
+  destination: "src/website/views/Content.tsx",
+  name: "Content",
+});
 
-export const FourOhFourReact: Module = React(
-  "src/website/views/FourOhFour.tsx",
-  "FourOhFour",
-);
+export const FourOhFourReact: Module = React({
+  destination: "src/website/views/FourOhFour.tsx",
+  name: "FourOhFour",
+});
 
-export const PageReact: Module = React(
-  "src/website/views/Page.tsx",
-  "Page",
-  [
+export const PageReact: Module = React({
+  destination: "src/website/views/Page.tsx",
+  name: "Page",
+  props: [
     Type.Required.new({
       name: "data",
       types: ["TopBarQuery", "null"],
     }),
   ],
-);
+});
 
-export const RootReact: Module = React(
-  "src/website/views/Root.tsx",
-  "Root",
-  [
+export const RootReact: Module = React({
+  destination: "src/website/views/Root.tsx",
+  name: "Root",
+  props: [
     Type.Required.new({
       name: "environment",
       type: "Environment",
     }),
   ],
-);
+});
