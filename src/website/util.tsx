1e5cd25313475f4bf98b274f4c4630fb94a3459f
Ari Chivukula <ari@chivuku.la>
2018-05-10 13:16:25 -0400

    remove electron

--- src/website/util.tsx
+++ src/website/util.tsx
@@ -2,34 +2,20 @@
  * This file is partially generated; only edit bespoke sections.
  *
  * SOURCE<<gen/website/util.ts::module>>
- * BESPOKE<<isElectron, goto>>
- * SIGNED<<Qlh1wB567St9Hxl0DcpiAIhlOW72voU/iN+BYp107o+AUPG+DM574WKZ69NeO3KLsx0Io9tPRalf0h4rfT3RyQ==>>
+ * BESPOKE<<goto>>
+ * SIGNED<<xmEAmBCNkmKkh5J4cChPNmzfMV9za8dykTqYkxcp6vXCxNnsX9pYsV8JaGtW9B+ec5kmTEUgb0MctNZrBsQX3Q==>>
  */
-
-export function isElectron(
-): boolean {
-  /* BESPOKE START <<isElectron>> */
-  return "process" in window;
-  /* BESPOKE END <<isElectron>> */
-}
 
 export function goto(
   url: string,
   samePage: boolean = false,
 ): void {
   /* BESPOKE START <<goto>> */
-  if (isElectron()) {
-    (window as any)
-      .require("electron")
-      .shell
-      .openExternal(url);
+  if (samePage) {
+    // @ts-ignore
+    window.location = url;
   } else {
-    if (samePage) {
-      // @ts-ignore
-      window.location = url;
-    } else {
-      window.open(url);
-    }
+    window.open(url);
   }
   /* BESPOKE END <<goto>> */
 }
