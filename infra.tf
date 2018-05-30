71d6f6229c7ce403f605fa4013f8d7909633fbb6
Ari Chivukula <ari@chivuku.la>
2018-05-30 14:32:28 -0700

    fix var access

--- infra.tf
+++ infra.tf
@@ -1,5 +1,17 @@
+variable "access_key" {
+  type = "string"
+}
+
+variable "secret_key" {
+  type = "string"
+}
+
+variable "region" {
+  type = "string"
+}
+
 provider "aws" {
-  access_key = "${access_key}"
-  secret_key = "${secret_key}"
-  region     = "${region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
