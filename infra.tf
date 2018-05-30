c511cffbeb2b16a5b9ee2129bbf7365362fc86ec
Ari Chivukula <ari@chivuku.la>
2018-05-30 14:35:32 -0700

    lambda fn

--- infra.tf
+++ infra.tf
@@ -10,8 +10,16 @@ variable "region" {
   type = "string"
 }
 
+variable "function_name" {
+  type = "string"
+}
+
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
+}
+
+data "aws_lambda_function" {
+  function_name = "${var.function_name}"
 }
