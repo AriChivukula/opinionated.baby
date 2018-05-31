8dd930db942b3126b33e1240e5d6b357c232122b
Ari Chivukula <ari@chivuku.la>
2018-05-31 12:47:08 -0700

    Add raven to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,7 @@
  *
  * SOURCE<<gen/server/index.ts::module>>
  * BESPOKE<<main, handler>>
- * SIGNED<<GagzrK4vqxx5QKRscwtlLUClCQUokSSMm8IiEZmF07N06+u7mF3TRhmTp9fonIAQHWFs/MLtE6L5xOZCpq6hCQ==>>
+ * SIGNED<<6b5kVh/2N+cJEEFsC5+ob8ojmdeO1BTCpvmWVeQ4jgojKfUbKJVh6Qd9fI42v72gpJER/Sye6/2kYLd/R2RG9g==>>
  */
 
 import "@babel/polyfill";
@@ -17,6 +17,7 @@ import cors from "cors";
 import express from "express";
 import bearer from "express-bearer-token";
 import helmet from "helmet";
+import Raven from "raven";
 
 import {
   genSetupDB,
@@ -33,6 +34,11 @@ const app: express.Express = express();
 let didSetup: boolean = false;
 
 /* BESPOKE START <<main>> */
+Raven.config(process.env.SENTRY)
+  .install();
+app.use(Raven.requestHandler());
+app.use(Raven.errorHandler());
+
 app.use(cors(), helmet(), bearer(), json(), urlencoded({ extended: true }));
 
 app.use((req: express.Request, res: express.Response, next: () => void): void => {
