2c8840ffb828229c99d773c95534c67e30ec6dae
Ari Chivukula <ari@chivuku.la>
2018-06-13 22:01:58 -0700

    Update screenshot.ts

--- src/__tests__/screenshot.ts
+++ src/__tests__/screenshot.ts
@@ -1,10 +1,11 @@
 import * as puppeteer from "puppeteer";
+import { makeSync } from "server/util";
 
-(async (): Promise<void> => {
+makeSync((async (): Promise<void> => {
   const browser: puppeteer.Browser = await puppeteer.launch();
   const page: puppeteer.Page = await browser.newPage();
   await page.goto("http://127.0.0.1:8080");
   await page.screenshot({path: "screenshot.png"});
 
   await browser.close();
-})();
+})());
