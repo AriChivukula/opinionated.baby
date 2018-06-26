5963d9444e46a72691a6a09178e3722489435f38
Ari Chivukula <ari@chivuku.la>
2018-06-25 21:56:38 -0700

    beta

--- src/website/index.tsx
+++ src/website/index.tsx
@@ -17,8 +17,6 @@ let apiURL: string;
 /* BESPOKE START <<main>> */
 if (window.location.hostname === "opinionated.baby") {
   apiURL = "https://api.opinionated.baby/graphql/opinionatedbaby/";
-} else if (window.location.hostname === "beta.opinionated.baby") {
-  apiURL = "https://api-beta.opinionated.baby/graphql/betaopinionatedbaby/";
 } else {
   apiURL = "http://127.0.0.1:8080/graphql/";
 }
