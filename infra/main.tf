9b272b075bd393f93c0bacaf3516e6c7580cdf3f
Ari Chivukula <ari@chivuku.la>
2018-06-25 22:35:08 -0700

    api

--- infra/main.tf
+++ infra/main.tf
@@ -47,16 +47,61 @@ resource "aws_api_gateway_rest_api" "ob_api" {
 }
 
 resource "aws_api_gateway_resource" "ob_resource" {
-  path_part   = "resource"
+  path_part   = "{proxy+}"
   parent_id   = "${aws_api_gateway_rest_api.ob_api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.ob_api.id}"
 }
 
-resource "aws_api_gateway_method" "ob_method" {
+resource "aws_api_gateway_method" "ob_method_proxy" {
   rest_api_id   = "${aws_api_gateway_rest_api.ob_api.id}"
   resource_id   = "${aws_api_gateway_resource.ob_resource.id}"
-  http_method   = "GET"
+  http_method   = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "ob_integration_proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.ob_api.id}"
+  resource_id = "${aws_api_gateway_method.ob_method_proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.ob_method_proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${data.aws_lambda_function.ob_lambda.invoke_arn}"
+}
+
+resource "aws_api_gateway_method" "ob_method_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.ob_api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.ob_api.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "ob_integration_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.ob_api.id}"
+  resource_id = "${aws_api_gateway_method.ob_method_root.resource_id}"
+  http_method = "${aws_api_gateway_method.ob_method_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${data.aws_lambda_function.ob_lambda.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "ob_deployment" {
+  depends_on = [
+    "aws_api_gateway_integration.ob_integration_proxy",
+    "aws_api_gateway_integration.ob_integration_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.ob_api.id}"
+  stage_name  = "PROD"
+}
+
+resource "aws_lambda_permission" "ob_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${local.name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_deployment.ob_deployment.execution_arn}/*/*"
 }
 
 resource "aws_cloudfront_distribution" "ob_distribution" {
