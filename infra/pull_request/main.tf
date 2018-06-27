8109627f0dffc2acf9b9ae86a3c350ef4ab08941
Ari Chivukula <ari@chivuku.la>
2018-06-26 23:27:09 -0700

    added root support

--- infra/pull_request/main.tf
+++ infra/pull_request/main.tf
@@ -88,7 +88,7 @@ resource "aws_api_gateway_method" "ob_method" {
 
 resource "aws_api_gateway_integration" "ob_integration" {
   rest_api_id = "${aws_api_gateway_rest_api.ob_api.id}"
-  resource_id = "${aws_api_gateway_resource.ob_resource.resource_id}"
+  resource_id = "${aws_api_gateway_method.ob_method.resource_id}"
   http_method = "${aws_api_gateway_method.ob_method.http_method}"
 
   integration_http_method = "ANY"
@@ -130,6 +130,18 @@ resource "aws_lambda_permission" "ob_permission" {
   function_name = "${var.NAME}-${var.BUILD}"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_deployment.ob_deployment.execution_arn}/*/*"
+}
+
+resource "aws_route53_record" "ob_record_dynamic" {
+  name    = "dynamic-${var.BUILD}.${var.DOMAIN}."
+  type    = "A"
+  zone_id = "${data.aws_route53_zone.ob_zone.zone_id}"
+
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_api_gateway_domain_name.ob_gateway.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.ob_gateway.cloudfront_zone_id}"
+  }
 }
 
 locals {
@@ -194,18 +206,6 @@ resource "aws_cloudfront_distribution" "ob_distribution" {
   }
 }
 
-resource "aws_route53_record" "ob_record_dynamic" {
-  name    = "dynamic-${var.BUILD}.${var.DOMAIN}."
-  type    = "A"
-  zone_id = "${data.aws_route53_zone.ob_zone.zone_id}"
-
-  alias {
-    evaluate_target_health = false
-    name                   = "${aws_cloudfront_distribution.ob_distribution.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.ob_distribution.hosted_zone_id}"
-  }
-}
-
 resource "aws_route53_record" "ob_record_static" {
   name    = "static-${var.BUILD}.${var.DOMAIN}."
   type    = "A"
@@ -213,7 +213,7 @@ resource "aws_route53_record" "ob_record_static" {
 
   alias {
     evaluate_target_health = false
-    name                   = "${aws_api_gateway_domain_name.ob_gateway.cloudfront_domain_name}"
-    zone_id                = "${aws_api_gateway_domain_name.ob_gateway.cloudfront_zone_id}"
+    name                   = "${aws_cloudfront_distribution.ob_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.ob_distribution.hosted_zone_id}"
   }
 }
