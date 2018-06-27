6e6db2c5501dad70a5df4b66fd1a6c8a0b433102
Ari Chivukula <ari@chivuku.la>
2018-06-26 20:30:54 -0700

    static prep

--- shell/awscli.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-export PATH=$HOME/.local/bin:$PATH
-pip install --user awscli
--- shell/verify.sh
+++ shell/verify.sh
@@ -1,4 +1,4 @@
 set -e
 
 npx gulp build
-"curl -H \"Authorization: token ${GITHUB_TOKEN}\" -X POST -d \"{\\\"body\\\": \\\"[Test build ${TRAVIS_BUILD_NUMBER}](https://to.do)\\\"}\" \"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments\""
+"curl -H \"Authorization: token ${GITHUB_TOKEN}\" -X POST -d \"{\\\"body\\\": \\\"[Test build ${TRAVIS_BUILD_NUMBER}](https://static-${TRAVIS_BUILD_NUMBER}.$DOMAIN)\\\"}\" \"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments\""
