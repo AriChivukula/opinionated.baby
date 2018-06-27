0e2d37eb7800dc3ec5d6960d69201481d5723c70
Ari Chivukula <ari@chivuku.la>
2018-06-26 21:10:44 -0700

    more vars

--- shell/actualize.sh
+++ shell/actualize.sh
@@ -11,7 +11,7 @@ npx gulp build
 npx jest -u build/2/website
 cp -R build/2/website/__tests__/__snapshots__ src/website/__tests__/__snapshots__
 terraform init infra/global
-terraform apply -state=infra/global/terraform.tfstate -var name=$NAME -var domain=$DOMAIN infra/global
+terraform apply -state=infra/global/terraform.tfstate infra/global
 npx ts-node ./node_modules/.bin/typeorm schema:sync
 git remote add target "https://arichiv:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 git add -A
