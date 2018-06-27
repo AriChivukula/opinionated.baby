65b81a0941253f7a34f718c54f73aa22285faced
Ari Chivukula <ari@chivuku.la>
2018-06-26 22:23:07 -0700

    var

--- shell/verify.sh
+++ shell/verify.sh
@@ -11,7 +11,6 @@ cp build/3/server/schema.graphql schema.graphql
 zip -q -r dynamic.zip node_modules index.js schema.graphql
 cp -R build/3/website static
 rm -rf index.js schema.graphql
-export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
 terraform init infra/pull_request
 terraform apply -state=infra/pull_request/terraform.tfstate infra/pull_request
 "curl -H \"Authorization: token ${GITHUB_TOKEN}\" -X POST -d \"{\\\"body\\\": \\\"[Test build ${TF_VAR_BUILD}](https://static-${TF_VAR_BUILD}.$TF_VAR_DOMAIN)\\\"}\" \"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments\""
