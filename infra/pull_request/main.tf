941faeadc022e04f23e3685aedd8153c447ca912
Ari Chivukula <ari@chivuku.la>
2018-06-26 22:17:15 -0700

    objects

--- infra/pull_request/main.tf
+++ infra/pull_request/main.tf
@@ -132,6 +132,17 @@ resource "aws_lambda_permission" "ob_permission" {
   source_arn    = "${aws_api_gateway_deployment.ob_deployment.execution_arn}/*/*"
 }
 
+locals {
+  files = ["index.html", "index.js", "index.css", "images/favicon.png", "images/v0.jpg", "images/v1.jpg", "images/v2.jpg", "images/v3.jpg", "images/v4.jpg", "images/v5.jpg", "images/v6.jpg", "images/v7.jpg", "images/v8.jpg"]
+}
+
+resource "aws_s3_bucket_object" "ob_object" {
+  count  = "${length(local.files)}"
+  bucket = "${var.NAME}"
+  key    = "${local.files[count.index]}"
+  source = "static/${local.files[count.index]}"
+}
+
 resource "aws_cloudfront_distribution" "ob_distribution" {
   enabled = true
 
