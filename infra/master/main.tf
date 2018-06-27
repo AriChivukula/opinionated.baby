078377dc6ed666c55fc6e3c5c833c7c6f723290a
Ari Chivukula <ari@chivuku.la>
2018-06-27 00:23:41 -0700

    fix deploy

--- infra/master/main.tf
+++ infra/master/main.tf
@@ -2,6 +2,10 @@ variable "NAME" {}
 
 provider "aws" {}
 
+locals {
+  files = ["index.html", "index.js", "index.css", "images/favicon.png", "images/v0.jpg", "images/v1.jpg", "images/v2.jpg", "images/v3.jpg", "images/v4.jpg", "images/v5.jpg", "images/v6.jpg", "images/v7.jpg", "images/v8.jpg"]
+}
+
 resource "aws_s3_bucket_object" "ob_object" {
   count  = "${length(local.files)}"
   bucket = "${var.NAME}"
