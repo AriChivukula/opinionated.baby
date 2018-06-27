248fc7147db83167f268558fd0bb5d872a7366a7
Ari Chivukula <ari@chivuku.la>
2018-06-26 19:00:22 -0700

    ignores

--- shell/terraform.sh
+++ shell/terraform.sh
@@ -2,8 +2,7 @@ set -e
 
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
-mv terraform /usr/local/bin/
 rm terraform_0.11.7_linux_amd64.zip
 cd infra
-terraform init
-terraform apply -auto-approve
+../terraform init
+../terraform apply -auto-approve
