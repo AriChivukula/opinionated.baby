0d34c429aa059850ccd02b13580890a20ba453f0
Ari Chivukula <ari@chivuku.la>
2018-06-27 10:15:14 -0700

    Update main.tf

--- infra/global/main.tf
+++ infra/global/main.tf
@@ -94,7 +94,7 @@ resource "aws_cloudfront_distribution" "ob_distribution" {
   }
 
   origin {
-    domain_name = "${aws_s3_bucket.ob_bucket.bucket_regional_domain_name}"
+    domain_name = "${aws_s3_bucket.ob_bucket.bucket_domain_name}"
     origin_id   = "${var.DOMAIN}"
     origin_path = "/master"
 
