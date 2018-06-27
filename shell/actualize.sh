d3f2e6f7d8e6ddddaa05c9b6e5c29ad595bf9792
Ari Chivukula <ari@chivuku.la>
2018-06-26 21:40:33 -0700

    "^1.6.0",

--- shell/actualize.sh
+++ shell/actualize.sh
@@ -6,7 +6,6 @@ cmp -s .outdated.new .outdated
 rm .outdated.new
 rm -rf src/website/__tests__/__snapshots__
 npx ts-node node_modules/.bin/typescriptase --files gen/**/*.ts
-npx relay-compiler --src src/ --schema src/server/schema.graphql --language typescript
 npx gulp build
 npx jest -u build/2/website
 cp -R build/2/website/__tests__/__snapshots__ src/website/__tests__/__snapshots__
