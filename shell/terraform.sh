c764705595ae3ba24644a74eec251640e989234e
Ari Chivukula <ari@chivuku.la>
2018-06-26 18:53:42 -0700

    sudo

--- shell/terraform.sh
+++ shell/terraform.sh
@@ -2,7 +2,7 @@ set -e
 
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
+mv terraform /usr/local/bin/
 rm terraform_0.11.7_linux_amd64.zip
 cd infra
 terraform init
