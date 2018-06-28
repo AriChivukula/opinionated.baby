17b45c46c34169c34b3d42b1d95ef38a744a7d0b
Ari Chivukula <ari@chivuku.la>
2018-06-28 13:06:17 -0700

    Update actualize.sh

--- shell/actualize.sh
+++ shell/actualize.sh
@@ -5,7 +5,7 @@ yarn ts-node node_modules/.bin/typescriptase --files gen/**/*.ts
 yarn relay-compiler --src src/ --schema src/server/schema.graphql --language typescript
 yarn gulp build || true
 yarn jest -u build/2/website || true
-cp -R build/2/website/__tests__/__snapshots__ src/website/__tests__/__snapshots__
+cp -R build/2/website/__tests__/__snapshots__ src/website/__tests__/__snapshots__  || true
 terraform init infra/global
 terraform apply -state=infra/global/terraform.tfstate -auto-approve infra/global
 git remote add target "https://arichiv:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
