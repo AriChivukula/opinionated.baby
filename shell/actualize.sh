f96822352341f971f6ae6587f5d7e61234cd24de
Ari Chivukula <ari@chivuku.la>
2018-06-28 09:28:38 -0700

    Update actualize.sh

--- shell/actualize.sh
+++ shell/actualize.sh
@@ -1,14 +1,14 @@
 set -e
 
 rm -rf src/website/__tests__/__snapshots__
-npx ts-node node_modules/.bin/typescriptase --files gen/**/*.ts
-npx relay-compiler --src src/ --schema src/server/schema.graphql --language typescript
-npx gulp build
-npx jest -u build/2/website
+yarn ts-node node_modules/.bin/typescriptase --files gen/**/*.ts
+yarn relay-compiler --src src/ --schema src/server/schema.graphql --language typescript
+yarn gulp build
+yarn jest -u build/2/website
 cp -R build/2/website/__tests__/__snapshots__ src/website/__tests__/__snapshots__
 terraform init infra/global
 terraform apply -state=infra/global/terraform.tfstate -auto-approve infra/global
-npx ts-node ./node_modules/.bin/typeorm schema:sync
+yarn ts-node ./node_modules/.bin/typeorm schema:sync
 git remote add target "https://arichiv:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 git add -A
 git commit -m "ACTUALIZE ${TRAVIS_BUILD_NUMBER}"
