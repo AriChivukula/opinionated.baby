08293a4e0a4f9d07b4f703f7a352ace86277ac45
Ari Chivukula <ari@chivuku.la>
2018-06-28 12:56:08 -0700

    Update tests.ts

--- gen/server/tests.ts
+++ gen/server/tests.ts
@@ -3,13 +3,6 @@ import {
   Module,
 } from "typescriptase";
 
-export const dbTest: Module = Jest({
-  destination: "src/server/__tests__/db.test.ts",
-  tests: [
-    "User",
-  ],
-});
-
 export const googleTest: Module = Jest({
   destination: "src/server/__tests__/google.test.ts",
   tests: [
