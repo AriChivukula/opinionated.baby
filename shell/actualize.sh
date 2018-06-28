cc60924f0dd11056753ac3533efb1746685cd143
Ari Chivukula <ari@chivuku.la>
2018-06-28 12:55:11 -0700

    Update actualize.sh

--- shell/actualize.sh
+++ shell/actualize.sh
@@ -8,7 +8,6 @@ yarn jest -u build/2/website || true
 cp -R build/2/website/__tests__/__snapshots__ src/website/__tests__/__snapshots__
 terraform init infra/global
 terraform apply -state=infra/global/terraform.tfstate -auto-approve infra/global
-yarn ts-node ./node_modules/.bin/typeorm schema:sync
 git remote add target "https://arichiv:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 git add -A
 git commit -m "ACTUALIZE ${TRAVIS_BUILD_NUMBER}"
