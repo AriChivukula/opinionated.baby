a7925d704afcca055ff5e66a7ff0c7ff2743a4c7
Ari Chivukula <ari@chivuku.la>
2018-06-28 17:39:27 -0700

    Update deploy.sh

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,7 +1,6 @@
 set -e
 
 bash shell/verify.sh
-rm -rf *.backup *.terraform *.tfstate
 export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
-terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/master
+terraform init -force-copy -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/master
 terraform apply -auto-approve infra/master
