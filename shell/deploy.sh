3f4032f83476278807299c0de238f9ed6d1a3a4f
Ari Chivukula <ari@chivuku.la>
2018-06-28 17:18:23 -0700

    Update deploy.sh

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,6 +1,12 @@
 set -e
 
-bash shell/verify.sh
+yarn gulp build
+yarn install --production=true --ignore-engines
+cp build/3/server/index.js index.js
+cp build/3/server/schema.graphql schema.graphql
+zip -q -r dynamic.zip node_modules index.js schema.graphql
+rm -rf index.js schema.graphql node_modules
+cp -R build/3/website static
 export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/master
-terraform apply -state=infra/master/terraform.tfstate -auto-approve infra/master
+terraform apply -auto-approve infra/master
