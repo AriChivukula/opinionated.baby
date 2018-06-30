8693ae915821439cec8a090b4d3d8d1ad7dbebcc
Ari Chivukula <ari@chivuku.la>
2018-06-30 15:29:07 -0700

    Update pr.sh

--- shell/pr.sh
+++ shell/pr.sh
@@ -1,8 +1,7 @@
 set -e
 
-yarn outdated
-bash shell/build.sh
 export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
-terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/${TF_VAR_BUILD}.tfstate" infra/pull_request
-terraform apply -auto-approve infra/pull_request
+bash shell/build.sh
+terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/${TF_VAR_BUILD}.tfstate" infra/pr
+terraform apply -auto-approve infra/pr
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"[Test ${TF_VAR_BUILD}](https://static-${TF_VAR_BUILD}.${TF_VAR_DOMAIN})\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
