33725800142307073e49be7634bc3e1d6f3ca8a4
Ari Chivukula <ari@chivuku.la>
2018-06-30 16:18:53 -0700

    Update cron.sh

--- shell/cron.sh
+++ shell/cron.sh
@@ -1,7 +1,7 @@
 set -e
 
 bash shell/build.sh
-for TF_VAR_BUILD in {1..1000}
+for TF_VAR_BUILD in {800..1000}
 do
   export TF_VAR_BUILD=$TF_VAR_BUILD
   echo "no" | terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/${TF_VAR_BUILD}.tfstate" infra/trigger || true
