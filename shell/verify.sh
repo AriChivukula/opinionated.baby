73dd950e153d9a26355cc0cd1a0ea0a312aeb751
Ari Chivukula <ari@chivuku.la>
2018-06-30 14:53:54 -0700

    Update verify.sh

--- shell/verify.sh
+++ shell/verify.sh
@@ -1,13 +1,7 @@
 set -e
 
 yarn outdated
-yarn gulp build
-yarn install --production=true --ignore-engines
-cp build/3/server/index.js index.js
-cp build/3/server/schema.graphql schema.graphql
-zip -q -r dynamic.zip node_modules index.js schema.graphql
-cp -R build/3/website static
-rm -rf index.js schema.graphql
+bash shell/build_and_install_site.sh
 export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/${TF_VAR_BUILD}.tfstate" infra/pull_request
 terraform apply -auto-approve infra/pull_request
