2b11cdd49a728c457818e128c5c14690fb84423f
Ari Chivukula <ari@chivuku.la>
2018-06-30 20:38:00 -0700

    Update pr.sh

--- shell/pr.sh
+++ shell/pr.sh
@@ -1,6 +1,7 @@
 set -e
 
 export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
+export TF_VAR_ID=$TRAVIS_BUILD_ID
 bash shell/build.sh
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/${TF_VAR_BUILD}.tfstate" infra/pr
 terraform apply -auto-approve infra/pr
