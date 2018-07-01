641caba4b735ac50153fb8ebbcb70406173c55c8
Ari Chivukula <ari@chivuku.la>
2018-06-30 20:38:09 -0700

    Update master.sh

--- shell/master.sh
+++ shell/master.sh
@@ -1,6 +1,7 @@
 set -e
 
 export TF_VAR_BUILD=master
+export TF_VAR_ID=$TRAVIS_BUILD_ID
 bash shell/build.sh
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/master
 terraform apply -auto-approve infra/master
