3312e4e1829e432559bd1fd6494359a372023711
Ari Chivukula <ari@chivuku.la>
2018-07-09 16:07:55 -0700

    Update master.sh

--- shell/master.sh
+++ shell/master.sh
@@ -1,6 +1,8 @@
 set -e
 
 export TF_VAR_BUILD=master
-bash shell/build.sh
+yarn gulp build
+yarn jest
+yarn codecov
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/master
 terraform apply -auto-approve infra/master
