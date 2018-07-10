fc1ec3eb3aff35de62587373f0fd462e5a91b612
Ari Chivukula <ari@chivuku.la>
2018-07-10 02:47:23 -0700

    Update trigger.sh

--- shell/trigger.sh
+++ shell/trigger.sh
@@ -1,5 +1,4 @@
 export TF_VAR_BUILD=$TRAVIS_BUILD_NUMBER
-rm -rf src/website/__tests__/__snapshots__
 yarn ts-node node_modules/.bin/typescriptase --files gen/**/*.ts
 yarn relay-compiler --src src/ --schema src/server/schema.graphql --language typescript
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/trigger.tfstate" infra/trigger
