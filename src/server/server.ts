5e0da6dc6c55f0273406cde945a8d562ed3c9e4b
Travis CI User <travis@example.org>
2018-07-10 22:49:21 +0000

    ACTUALIZE 1580

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,11 +3,11 @@
  *
  * SOURCE<<gen/server/server.ts::module>>
  * BESPOKE<<servers>>
- * SIGNED<<7fRMp7i3q1DMSimft7hKNcwBCcvls9ODKw6EM6H0vAGGjSWaZMFclxbwUeGpG9E87hvADmgxl2+OK35da/BSKA==>>
+ * SIGNED<<t793RpMk6FtnvSaTcr+3R8567fAo8eBMb2ony7sKsJEd5ozmmrbxGArMftgPuAIYfJAK2zPUCoopPhhRAN2OYw==>>
  */
 
-import * as express from "express";
-import * as graphqlHTTP from "express-graphql";
+import express from "express";
+import graphqlHTTP from "express-graphql";
 import {
   readFileSync,
 } from "fs";
