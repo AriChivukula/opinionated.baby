25c71d888261b7020260bac3296df6134d0f921b
Travis CI User <travis@example.org>
2018-07-10 18:18:05 +0000

    ACTUALIZE 1450

--- src/server/__tests__/google.test.ts
+++ src/server/__tests__/google.test.ts
@@ -2,63 +2,9 @@
  * This file is partially generated; only edit bespoke sections.
  *
  * SOURCE<<gen/server/tests.ts::googleTest>>
- * BESPOKE<<imports, beforeAll, afterAll, beforeEach, afterEach, getLoginURL, genAccessTokenInfo>>
- * SIGNED<<vUOisHxAamMRwL8PCqh8BOBLLdOL6noNhhx1i+ZimSYE+PXqLUuoyBT2bYBt7m2eGss6ydIbdDbyDE5UYx+1fA==>>
+ * BESPOKE<<test>>
+ * SIGNED<<4MD6HpCxEHJEieR4CvNaKXz0/Dkn5A4pwUpvUeSMYjRxHMbKdAMlTqAdsfWS0klw+sJtm2qRxU5a0lVikTwbKg==>>
  */
 
-/* BESPOKE START <<imports>> */
-import {
-  genAccessTokenInfo,
-  getLoginURL,
-} from "../google";
-/* BESPOKE END <<imports>> */
-
-beforeAll(
-  async (): Promise<void> => {
-    /* BESPOKE START <<beforeAll>> */
-    /* BESPOKE END <<beforeAll>> */
-  },
-);
-
-afterAll(
-  async (): Promise<void> => {
-    /* BESPOKE START <<afterAll>> */
-    /* BESPOKE END <<afterAll>> */
-  },
-);
-
-beforeEach(
-  async (): Promise<void> => {
-    /* BESPOKE START <<beforeEach>> */
-    /* BESPOKE END <<beforeEach>> */
-  },
-);
-
-afterEach(
-  async (): Promise<void> => {
-    /* BESPOKE START <<afterEach>> */
-    /* BESPOKE END <<afterEach>> */
-  },
-);
-
-test(
-  "getLoginURL",
-  async (): Promise<void> => {
-    /* BESPOKE START <<getLoginURL>> */
-    const url: string = getLoginURL();
-    expect(url)
-      .toMatch("/accounts.google.com/");
-    /* BESPOKE END <<getLoginURL>> */
-  },
-);
-
-test(
-  "genAccessTokenInfo",
-  async (): Promise<void> => {
-    /* BESPOKE START <<genAccessTokenInfo>> */
-    await expect(genAccessTokenInfo("ERROR"))
-      .rejects
-      .toThrow(Error);
-    /* BESPOKE END <<genAccessTokenInfo>> */
-  },
-);
+/* BESPOKE START <<test>> */
+/* BESPOKE END <<test>> */
