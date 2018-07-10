77bd05242ee5b6c09b00a6b3d91741b38451a71a
Ari Chivukula <ari@chivuku.la>
2018-07-10 11:55:24 -0700

    Update and rename src/server/test/google.ts to src/test/google.ts

--- src/test/google.ts
+++ src/test/google.ts
@@ -1,7 +1,7 @@
 /**
  * This file is partially generated; only edit bespoke sections.
  *
- * SOURCE<<gen/server/tests.ts::googleTest>>
+ * SOURCE<<gen/test/tests.ts::googleTest>>
  * BESPOKE<<test>>
  * SIGNED<<4MD6HpCxEHJEieR4CvNaKXz0/Dkn5A4pwUpvUeSMYjRxHMbKdAMlTqAdsfWS0klw+sJtm2qRxU5a0lVikTwbKg==>>
  */
@@ -15,7 +15,7 @@ import * as chaiAsPromised from "chai-as-promised";
 import {
   genAccessTokenInfo,
   getLoginURL,
-} from "../google";
+} from "../server/google";
 
 chai.use(chaiAsPromised);
 
