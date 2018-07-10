3b4becb130148819eb83da467d855ff4c40d3902
Ari Chivukula <ari@chivuku.la>
2018-07-10 11:18:52 -0700

    Update google.test.ts

--- src/server/__tests__/google.test.ts
+++ src/server/__tests__/google.test.ts
@@ -7,4 +7,21 @@
  */
 
 /* BESPOKE START <<test>> */
+test(
+  "getLoginURL",
+  async (): Promise<void> => {
+    const url: string = getLoginURL();
+    expect(url)
+      .toMatch("/accounts.google.com/");
+  },
+);
+
+test(
+  "genAccessTokenInfo",
+  async (): Promise<void> => {
+    await expect(genAccessTokenInfo("ERROR"))
+      .rejects
+      .toThrow(Error);
+  },
+);
 /* BESPOKE END <<test>> */
