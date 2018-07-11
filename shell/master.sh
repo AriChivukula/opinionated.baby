e2b07e356283457b0ecbd2da1ecff4e29f025cdc
Ari Chivukula <ari@chivuku.la>
2018-07-10 18:50:25 -0700

    Update master.sh

--- shell/master.sh
+++ shell/master.sh
@@ -5,3 +5,4 @@ bash shell/test.sh
 bash shell/build.sh
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/master
 terraform apply -auto-approve infra/master
+curl https://api.rollbar.com/api/1/deploy/ -F access_token=$TF_VAR_ROLLBAR_CLIENT -F environment=production -F revision=`git log -n 1 --pretty=format:"%H"` -F local_username=`whoami`
