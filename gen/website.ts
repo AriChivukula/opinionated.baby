3f49d6fd8501e89df26edcec87644b2cebc3a2bd
Ari Chivukula <ari@chivuku.la>
2018-07-12 10:43:54 -0700

    Update website.ts

--- gen/website.ts
+++ gen/website.ts
@@ -82,3 +82,53 @@ export const TopBar: Module = React({
   relayMutation: true,
   relayType: ERelayType.FRAGMENT,
 });
+
+export const Website: Module = Module.new({
+  content: [
+    Import.new({
+      name: "js-cookie",
+      withAllAs: "cookie",
+    }),
+    Import.new({
+      name: "react",
+      withAllAs: "React",
+    }),
+    Import.new({
+      name: "react-dom",
+      withAllAs: "ReactDOM",
+    }),
+    Import.new({
+      name: "react-relay-network-modern",
+      with: ["authMiddleware", "RelayNetworkLayer", "urlMiddleware"],
+    }),
+    Import.new({
+      name: "react-router-dom",
+      with: ["BrowserRouter", "Route", "Switch"],
+    }),
+    Import.new({
+      name: "relay-runtime",
+      with: ["Environment", "RecordSource", "Store"],
+    }),
+    Import.new({
+      name: "./views/FourOhFour",
+      with: ["FourOhFour"],
+    }),
+    Import.new({
+      name: "./views/Root",
+      with: ["Root"],
+    }),
+    Function.Sync.newExported({
+      content: [
+        Bespoke.new({
+          name: "render",
+        }),
+      ],
+      inTypes: [
+        Type.Argument.new({ name: "apiURL", type: "string" }),
+      ],
+      name: "render",
+      outType: Type.Anonymous.new({ type: "void" }),
+    }),
+  ],
+  destination: "src/website/website.tsx",
+});
