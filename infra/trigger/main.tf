b9624d679c490df249f35dbe7c472304758222de
Ari Chivukula <ari@chivuku.la>
2018-07-12 16:34:49 -0700

    Update main.tf

--- infra/trigger/main.tf
+++ infra/trigger/main.tf
@@ -49,15 +49,15 @@ resource "aws_security_group" "ob_security" {
   vpc_id = "${aws_vpc.ob_vpc.id}"
 
   ingress {
-     from_port = 0
-    to_port = 0
+    from_port = -1
+    to_port = -1
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
+    from_port = -1
+    to_port = -1
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
