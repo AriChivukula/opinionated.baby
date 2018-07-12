f614c331444d9f0546ba3c9ab25a90920cf0ce15
Ari Chivukula <ari@chivuku.la>
2018-07-12 14:00:08 -0700

    Update server.ts

--- gen/server.ts
+++ gen/server.ts
@@ -1,244 +1,46 @@
 import { Bespoke, Function, Import, Interface, Module, Type, Variable } from "typescriptase";
 
-export const Server: Module = Module.new({
+export const server: Module = Module.new({
   content: [
-    Import.new({
-      name: "express",
-      withDefaultAs: "express",
-    }),
-    Import.new({
-      name: "express-graphql",
-      withDefaultAs: "graphqlHTTP",
-    }),
-    Import.new({
-      name: "fs",
-      with: ["readFileSync"],
-    }),
-    Import.new({
-      name: "graphql",
-      with: ["buildSchema", "GraphQLSchema"],
-    }),
-    Import.new({
-      name: "path",
-      with: ["join"],
-    }),
-    Import.new({
-      name: "./root",
-      with: ["genRoot"],
-    }),
     Bespoke.new({
-      name: "servers",
+      name: "server",
     }),
   ],
   destination: "src/server/server.ts",
 });
 
-export const Google: Module = Module.new({
+export const google: Module = Module.new({
   content: [
-    Import.new({
-      name: "google-auth-library",
-      with: ["OAuth2Client"],
-    }),
-    Import.new({
-      name: "googleapis",
-      with: ["google"],
-    }),
-    Interface.newExported({
-      name: "IAccessToken",
-      types: [
-        Type.Required.new({ name: "tokens", type: "{ access_token?: string | null }" }),
-      ],
-    }),
-    Interface.newExported({
-      name: "IAccessTokenInfo",
-      types: [
-        Type.Required.new({ name: "data", type: "{ email?: string | undefined; user_id?: string | undefined; verified_email?: boolean | undefined }" }),
-      ],
-    }),
-    Function.Sync.newInternal({
-      content: [
-        Bespoke.new({
-          name: "getOAuthClient",
-        }),
-      ],
-      inTypes: [],
-      name: "getOAuthClient",
-      outType: Type.Anonymous.new({ type: "OAuth2Client" }),
-    }),
-    Function.Sync.newExported({
-      content: [
-        Bespoke.new({
-          name: "getLoginURL",
-        }),
-      ],
-      inTypes: [],
-      name: "getLoginURL",
-      outType: Type.Anonymous.new({ type: "string" }),
-    }),
-    Function.Async.newExported({
-      content: [
-        Bespoke.new({
-          name: "genAccessToken",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "code", type: "string" }),
-      ],
-      name: "genAccessToken",
-      outType: Type.Anonymous.new({ type: "Promise<string>" }),
-    }),
-    Function.Async.newExported({
-      content: [
-        Bespoke.new({
-          name: "genAccessTokenInfo",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "accessToken", type: "string" }),
-      ],
-      name: "genAccessTokenInfo",
-      outType: Type.Anonymous.new({ type: "Promise<IAccessTokenInfo>" }),
-    }),
-    Function.Async.newExported({
-      content: [
-        Bespoke.new({
-          name: "genUserForAccessToken",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "accessToken", type: "string" }),
-      ],
-      name: "genUserForAccessToken",
-      outType: Type.Anonymous.new({ type: "Promise<object>" }),
+    Bespoke.new({
+      name: "google",
     }),
   ],
   destination: "src/server/google.ts",
 });
 
-export const Index: Module = Module.new({
+export const index: Module = Module.new({
   content: [
-    Import.new({
-      name: "@babel/polyfill",
-    }),
-    Import.new({
-      name: "aws-serverless-express",
-      withAllAs: "lambda",
-    }),
-    Import.new({
-      name: "rollbar",
-      withAllAs: "Rollbar",
-    }),
-    Import.new({
-      name: "body-parser",
-      with: ["json", "urlencoded"],
-    }),
-    Import.new({
-      name: "cors",
-      withDefaultAs: "cors",
-    }),
-    Import.new({
-      name: "express",
-      withDefaultAs: "express",
-    }),
-     Import.new({
-      name: "express-bearer-token",
-      withDefaultAs: "bearer",
-    }),
-    Import.new({
-      name: "helmet",
-      withDefaultAs: "helmet",
-    }),
-    Import.new({
-      name: "./server",
-      with: ["graphQL"],
-    }),
-    Variable.newImmutable({
-      assignment: "express()",
-      type: Type.Required.new({ name: "app", type: "express.Express" }),
-    }),
-    Variable.newMutable({
-      assignment: "false",
-      type: Type.Required.new({ name: "didSetup", type: "boolean" }),
-    }),
     Bespoke.new({
-      name: "main",
-    }),
-    Function.Sync.newExported({
-      content: [
-        Bespoke.new({
-          name: "handler",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "event", type: "object" }),
-        Type.Argument.new({ name: "context", type: "object" }),
-      ],
-      name: "handler",
-      outType: Type.Anonymous.new({ type: "void" }),
+      name: "index",
     }),
   ],
   destination: "src/server/index.ts",
 });
 
-export const Root: Module = Module.new({
+export const root: Module = Module.new({
   content: [
-    Import.new({
-      name: "express",
-      withAllAs: "express",
-    }),
-    Import.new({
-      name: "./google",
-      with: ["genAccessToken", "getLoginURL", "genUserForAccessToken"],
-    }),
-    Import.new({
-      name: "./util",
-      with: ["genNullOnThrow"],
-    }),
-    Function.Async.newExported({
-      content: [
-        Bespoke.new({
-          name: "genRoot",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "req", type: "express.Request" }),
-        Type.Argument.new({ name: "res", type: "express.Response" }),
-      ],
-      name: "genRoot",
-      outType: Type.Anonymous.new({ type: "Promise<object>" }),
+    Bespoke.new({
+      name: "root",
     }),
   ],
   destination: "src/server/root.ts",
 });
 
-export const Util: Module = Module.new({
+export const utility: Module = Module.new({
   content: [
-    Function.Sync.newExported({
-      content: [
-        Bespoke.new({
-          name: "makeSync<T>",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "wasAsync", type: "Promise<T>" }),
-      ],
-      name: "makeSync",
-      outType: Type.Anonymous.new({ type: "void" }),
-      templates: ["T"],
-    }),
-    Function.Async.newExported({
-      content: [
-        Bespoke.new({
-          name: "genNullOnThrow<T>",
-        }),
-      ],
-      inTypes: [
-        Type.Argument.new({ name: "mightThrow", type: "() => Promise<T>" }),
-      ],
-      name: "genNullOnThrow",
-      outType: Type.Anonymous.new({ type: "Promise<T | null>" }),
-      templates: ["T"],
+    Bespoke.new({
+      name: "utility",
     }),
   ],
-  destination: "src/server/util.ts",
+  destination: "src/server/utility.ts",
 });
