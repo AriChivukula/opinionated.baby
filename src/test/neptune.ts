32b7dd938141637b08cd576066ddf7d7721599be
Travis CI User <travis@example.org>
2018-07-12 17:53:35 +0000

    ACTUALIZE 1658

--- src/test/neptune.ts
+++ src/test/neptune.ts
@@ -1,7 +1,7 @@
 /**
  * This file is partially generated; only edit bespoke sections.
  *
- * SOURCE<<gen/test.ts::neptune>>
+ * SOURCE<<gen/test.ts::Neptune>>
  * BESPOKE<<test>>
  * SIGNED<<4MD6HpCxEHJEieR4CvNaKXz0/Dkn5A4pwUpvUeSMYjRxHMbKdAMlTqAdsfWS0klw+sJtm2qRxU5a0lVikTwbKg==>>
  */
