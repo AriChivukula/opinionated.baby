19533aceddb2f8f9154f8821f20861ae1137c115
Ari Chivukula <ari@chivuku.la>
2018-07-12 14:10:09 -0700

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,4 +7,66 @@
  */
 
 /* BESPOKE START <<custom>> */
+import "@babel/polyfill";
+
+import * as lambda from "aws-serverless-express";
+import {
+  json,
+  urlencoded,
+} from "body-parser";
+import cors from "cors";
+import express from "express";
+import bearer from "express-bearer-token";
+import helmet from "helmet";
+import * as Rollbar from "rollbar";
+
+import {
+  graphQL,
+} from "./server";
+
+const app: express.Express = express();
+
+let didSetup: boolean = false;
+
+app.use(cors(), helmet(), bearer(), json(), urlencoded({ extended: true }));
+
+app.use((req: express.Request, res: express.Response, next: () => void): void => {
+  if (!didSetup) {
+    didSetup = true;
+  }
+  next();
+});
+
+app.use("/graphql", graphQL);
+
+app.use((new Rollbar({
+  accessToken: process.env.TF_VAR_ROLLBAR_SERVER,
+  verbose: true,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})).errorHandler());
+
+export function handler(
+  event: object,
+  context: object,
+): void {
+  lambda.proxy(
+    lambda.createServer(
+      app,
+      undefined,
+      [
+        "application/octet-stream",
+        "font/eot",
+        "font/opentype",
+        "font/otf",
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+      ],
+    ),
+    event,
+    // @ts-ignore
+    context,
+  );
+}
 /* BESPOKE END <<custom>> */
