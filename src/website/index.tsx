b67ed1a428b889314ab60a0ae515e5be82174a91
Travis CI User <travis@example.org>
2018-07-12 21:16:56 +0000

    ACTUALIZE 1742

--- src/website/index.tsx
+++ src/website/index.tsx
@@ -1,21 +1,10 @@
 /**
  * This file is partially generated; only edit bespoke sections.
  *
- * SOURCE<<gen/website.ts::Index>>
- * BESPOKE<<main>>
- * SIGNED<<baly0HNt5Vcby1yNztL0ZF2baT+6f3zBSkNYyarovz4LGwdM1QXeRCloIsWeq/4Lmtuv49VyU2mmx9Uo9kgvNQ==>>
+ * SOURCE<<gen/website.ts::index>>
+ * BESPOKE<<custom>>
+ * SIGNED<<J9FCy5Eb9w0afcgAaRFGr3gxQY8sbFhMkYnvR4PLOLGutLf1jQN3nT49Pgi/UgT8KqpaBJanNZoyEo3inVMTZQ==>>
  */
 
-import "@babel/polyfill";
-
-import {
-  render,
-} from "./website";
-
-let apiURL: string;
-
-/* BESPOKE START <<main>> */
-window.onload = (): void => {
-  render("https://dynamic-ENV_BUILD.ENV_DOMAIN/graphql/ENV_NAME-ENV_BUILD/");
-};
-/* BESPOKE END <<main>> */
+/* BESPOKE START <<custom>> */
+/* BESPOKE END <<custom>> */
--- src/website/utility.tsx
+++ src/website/utility.tsx
@@ -1,7 +1,7 @@
 /**
  * This file is partially generated; only edit bespoke sections.
  *
- * SOURCE<<gen/website.ts::website>>
+ * SOURCE<<gen/website.ts::utility>>
  * BESPOKE<<custom>>
  * SIGNED<<J9FCy5Eb9w0afcgAaRFGr3gxQY8sbFhMkYnvR4PLOLGutLf1jQN3nT49Pgi/UgT8KqpaBJanNZoyEo3inVMTZQ==>>
  */
