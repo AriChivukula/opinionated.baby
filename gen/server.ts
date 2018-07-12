53e3158488dbbe5d5cf29f6724c499b19623b29b
Ari Chivukula <ari@chivuku.la>
2018-07-12 10:46:27 -0700

    Update server.ts

--- gen/server.ts
+++ gen/server.ts
@@ -1,4 +1,4 @@
-import { Bespoke, Function, Import, Interface, Module, Type } from "typescriptase";
+import { Bespoke, Function, Import, Interface, Module, Type, Variable } from "typescriptase";
 
 export const Server: Module = Module.new({
   content: [
@@ -33,7 +33,7 @@ export const Server: Module = Module.new({
   destination: "src/server/server.ts",
 });
 
-export const module: Module = Module.new({
+export const Google: Module = Module.new({
   content: [
     Import.new({
       name: "google-auth-library",
@@ -113,4 +113,69 @@ export const module: Module = Module.new({
     }),
   ],
   destination: "src/server/google.ts",
+});
+
+export const Index: Module = Module.new({
+  content: [
+    Import.new({
+      name: "@babel/polyfill",
+    }),
+    Import.new({
+      name: "aws-serverless-express",
+      withAllAs: "lambda",
+    }),
+    Import.new({
+      name: "rollbar",
+      withAllAs: "Rollbar",
+    }),
+    Import.new({
+      name: "body-parser",
+      with: ["json", "urlencoded"],
+    }),
+    Import.new({
+      name: "cors",
+      withDefaultAs: "cors",
+    }),
+    Import.new({
+      name: "express",
+      withDefaultAs: "express",
+    }),
+     Import.new({
+      name: "express-bearer-token",
+      withDefaultAs: "bearer",
+    }),
+    Import.new({
+      name: "helmet",
+      withDefaultAs: "helmet",
+    }),
+    Import.new({
+      name: "./server",
+      with: ["graphQL"],
+    }),
+    Variable.newImmutable({
+      assignment: "express()",
+      type: Type.Required.new({ name: "app", type: "express.Express" }),
+    }),
+    Variable.newMutable({
+      assignment: "false",
+      type: Type.Required.new({ name: "didSetup", type: "boolean" }),
+    }),
+    Bespoke.new({
+      name: "main",
+    }),
+    Function.Sync.newExported({
+      content: [
+        Bespoke.new({
+          name: "handler",
+        }),
+      ],
+      inTypes: [
+        Type.Argument.new({ name: "event", type: "object" }),
+        Type.Argument.new({ name: "context", type: "object" }),
+      ],
+      name: "handler",
+      outType: Type.Anonymous.new({ type: "void" }),
+    }),
+  ],
+  destination: "src/server/index.ts",
 });
