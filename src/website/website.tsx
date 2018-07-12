d75021adf00fbbff3b20d4a4cd319bc3b41dd7c7
Ari Chivukula <ari@chivuku.la>
2018-07-12 14:22:28 -0700

    Update website.tsx

--- src/website/website.tsx
+++ src/website/website.tsx
@@ -7,4 +7,55 @@
  */
 
 /* BESPOKE START <<custom>> */
+import * as cookie from "js-cookie";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+  authMiddleware,
+  RelayNetworkLayer,
+  urlMiddleware,
+} from "react-relay-network-modern";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from "react-router-dom";
+import {
+  Environment,
+  RecordSource,
+  Store,
+} from "relay-runtime";
+
+import {
+  FourOhFour,
+} from "./views/FourOhFour";
+import {
+  Root,
+} from "./views/Root";
+
+export function render(
+  apiURL: string,
+): void {
+  const environment: Environment = new Environment({
+    network: new RelayNetworkLayer([
+      urlMiddleware({ url: apiURL }),
+      authMiddleware({
+        allowEmptyToken: true,
+        token: (): string => cookie.get("accessToken") || "",
+      }),
+    ]),
+    store: new Store(new RecordSource()),
+  });
+  const renderer: () => JSX.Element = (): JSX.Element => <Root environment={environment} />;
+  ReactDOM.render(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={renderer} />
+        <Route path="/index.html" render={renderer} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </BrowserRouter>,
+    document.getElementById("root"),
+  );
+}
 /* BESPOKE END <<custom>> */
