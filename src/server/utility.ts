0a1f15eb4d927defab2394cc53490659120e1a5d
Ari Chivukula <ari@chivuku.la>
2018-07-12 14:12:52 -0700

    Update utility.ts

--- src/server/utility.ts
+++ src/server/utility.ts
@@ -7,4 +7,32 @@
  */
 
 /* BESPOKE START <<custom>> */
+export function makeSync<T>(
+  wasAsync: Promise<T>,
+): void {
+  wasAsync
+    .catch((err: Error): void => {
+      console.log(err);
+    })
+    .then((): void => {
+      return;
+    })
+    .catch((err: Error): void => {
+      console.log(err);
+    });
+
+  return;
+}
+
+export async function genNullOnThrow<T>(
+  mightThrow: () => Promise<T>,
+): Promise<T | null> {
+  try {
+    return await mightThrow();
+  } catch (error) {
+    console.log(error);
+
+    return null;
+  }
+}
 /* BESPOKE END <<custom>> */
