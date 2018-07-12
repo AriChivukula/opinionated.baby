2641cde9eb7b6f2f398fd28c57b1aeb6ed4aa812
Ari Chivukula <ari@chivuku.la>
2018-07-12 10:18:30 -0700

    Create neptune.ts

---
+++ gen/server/neptune.ts
@@ -0,0 +1,9 @@
+import { Bespoke, Module } from "typescriptase";
+
+export const module: Module = Module.new({
+  content: [
+    Bespoke.new({
+      name: "neptune",
+    }),
+  destination: "src/server/neptune.ts",
+});
