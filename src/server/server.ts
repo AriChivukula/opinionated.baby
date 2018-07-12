22d1d1a9758a7c9f152d38072aa0b7f0fe29f265
Ari Chivukula <ari@chivuku.la>
2018-07-12 14:11:44 -0700

    Update server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,4 +7,32 @@
  */
 
 /* BESPOKE START <<custom>> */
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import {
+  readFileSync,
+} from "fs";
+import {
+  buildSchema,
+  GraphQLSchema,
+} from "graphql";
+import {
+  join,
+} from "path";
+
+import {
+  genRoot,
+} from "./root";
+
+const schema: GraphQLSchema = buildSchema(
+  readFileSync(join(__dirname, "schema.graphql"), "ascii"),
+);
+
+export const graphQL: graphqlHTTP.Middleware = graphqlHTTP(
+  async (req: express.Request, res: express.Response): Promise<graphqlHTTP.OptionsData> => ({
+    graphiql: true,
+    rootValue: await genRoot(req, res),
+    schema,
+  }),
+);
 /* BESPOKE END <<custom>> */
