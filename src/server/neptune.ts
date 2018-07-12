6ba5378094f3847cd87a6f9bda27bca0030c0a1a
Travis CI User <travis@example.org>
2018-07-12 17:26:39 +0000

    ACTUALIZE 1608

---
+++ src/server/neptune.ts
@@ -0,0 +1,10 @@
+/**
+ * This file is partially generated; only edit bespoke sections.
+ *
+ * SOURCE<<gen/server/neptune.ts::module>>
+ * BESPOKE<<neptune>>
+ * SIGNED<<nOlimrt839ENNm8TMbTA8w98ztf0unb/75FccZIPXmBPl8a10p9qJ90J+OdihON1RhP8yiAT26fRwmNpoNYPyQ==>>
+ */
+
+/* BESPOKE START <<neptune>> */
+/* BESPOKE END <<neptune>> */
