86222c297ba11311cd84f251b59292cca98a1f21
Ari Chivukula <ari@chivuku.la>
2018-07-13 00:16:02 -0700

    Update main.tf

--- infra/master/main.tf
+++ infra/master/main.tf
@@ -75,7 +75,7 @@ resource "aws_lambda_function" "ob_lambda" {
   }
   
   vpc_config {
-    subnet_ids = ["${data.aws_subnet_ids.ob_subnet.id}"]
+    subnet_ids = ["${data.aws_subnet_id.ob_subnet.id}"]
     security_group_ids = ["${data.aws_security_group.ob_security.id}"]
   }
 
