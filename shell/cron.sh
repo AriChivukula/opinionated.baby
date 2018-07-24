b8cab856c908186ef510bc738851290be1fcc8b2
Ari Chivukula <ari@chivuku.la>
2018-07-24 13:19:12 -0700

    Update cron.sh

--- shell/cron.sh
+++ shell/cron.sh
@@ -1,7 +1,7 @@
 set -e
 
 export TF_VAR_BRANCH=$TRAVIS_BRANCH
-export LOCAL_DOMAIN=TEMP.$DOMAIN
+export TF_VAR_LOCAL_DOMAIN=TEMP.$DOMAIN
 bash shell/build.sh
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/PR_*.tfstate" infra/pr || true
 terraform destroy -auto-approve infra/pr || true
