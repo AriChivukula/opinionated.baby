79db507a2ef392963d52717304d182f03324936c
Ari Chivukula <ari@chivuku.la>
2018-07-23 18:15:14 -0700

    Update Content.tsx

--- src/website/views/Content.tsx
+++ src/website/views/Content.tsx
@@ -48,74 +48,74 @@ class __Content extends React.Component<IContentProps> {
   public render(
   ): JSX.Element {
     /* BESPOKE START <<render>> */
-  return (
-    <Grid>
-      <GridCell span={12}>
-        <Typography use="headline2" tag="div">
-          A confidently immature starting point
-        </Typography>
-      </GridCell>
-      <GridCell span={12}>
-        <Typography use="body1" tag="div">
-          The hardest part of building a new dynamic website is avoiding
-          all the dragons. Starting a development environment can freeze us
-          with choice overload.
-          {" "}
-          <Typography
-            theme="text-secondary-on-background"
-            onClick={(): void => { goto("https://github.com/AriChivukula/opinionated.baby/"); }}>
-            Opinionated Baby
+    return (
+      <Grid>
+        <GridCell span={12}>
+          <Typography use="headline2" tag="div">
+            A confidently immature starting point
           </Typography>
-          {" "}
-          solves this by moving the starting line past the quagmire of setup
-          and into the pleasure of production logic.
-        </Typography>
-      </GridCell>
-      <GridCell span={12}>
-        <Typography use="headline2" tag="div">
-          Important choices I made for you
-        </Typography>
-      </GridCell>
-      {renderContent()}
-      <GridCell span={12}>
-        <Typography use="headline2" tag="div">
-          <span style={{textDecoration: "line-through"}}>
-            Self justification
-          </span>
-          {" "}
-          Philosophy
-        </Typography>
-      </GridCell>
-      <GridCell span={12}>
-        <Typography use="body1" tag="div">
-          This framework is one-size-fits-me. It's an open source
-          version of a system I use when building complex websites.
-          Anyone may attempt to use, critique, or contribute to it. The
-          consistency with which feedback is incorporated will be nothing
-          short of capricious. My general mission and purpose is to support
-          the vitality and happy, healthy development of our
-          {" "}
-          <Typography
-            theme="text-secondary-on-background"
-            onClick={(): void => { goto("http://www.nic.baby/policies.html"); }}>
-            babies and children
+        </GridCell>
+        <GridCell span={12}>
+          <Typography use="body1" tag="div">
+            The hardest part of building a new dynamic website is avoiding
+            all the dragons. Starting a development environment can freeze us
+            with choice overload.
+            {" "}
+            <Typography
+              theme="text-secondary-on-background"
+              onClick={(): void => { goto("https://github.com/AriChivukula/opinionated.baby/"); }}>
+              Opinionated Baby
+            </Typography>
+            {" "}
+            solves this by moving the starting line past the quagmire of setup
+            and into the pleasure of production logic.
           </Typography>
-          .
-        </Typography>
-      </GridCell>
-      <GridCell span={12}>
-        <Typography use="headline2" tag="div">
-          Release history
-        </Typography>
-      </GridCell>
-      {renderReleases()}
-    </Grid>
-  );
+        </GridCell>
+        <GridCell span={12}>
+          <Typography use="headline2" tag="div">
+            Important choices I made for you
+          </Typography>
+        </GridCell>
+        {renderContent()}
+        <GridCell span={12}>
+          <Typography use="headline2" tag="div">
+            <span style={{textDecoration: "line-through"}}>
+              Self justification
+            </span>
+            {" "}
+            Philosophy
+          </Typography>
+        </GridCell>
+        <GridCell span={12}>
+          <Typography use="body1" tag="div">
+            This framework is one-size-fits-me. It's an open source
+            version of a system I use when building complex websites.
+            Anyone may attempt to use, critique, or contribute to it. The
+            consistency with which feedback is incorporated will be nothing
+            short of capricious. My general mission and purpose is to support
+            the vitality and happy, healthy development of our
+            {" "}
+            <Typography
+              theme="text-secondary-on-background"
+              onClick={(): void => { goto("http://www.nic.baby/policies.html"); }}>
+              babies and children
+            </Typography>
+            .
+          </Typography>
+        </GridCell>
+        <GridCell span={12}>
+          <Typography use="headline2" tag="div">
+            Release history
+          </Typography>
+        </GridCell>
+        {renderReleases()}
+      </Grid>
+    );
     /* BESPOKE END <<render>> */
   }
 
   /* BESPOKE START <<implementation>> */
-  function renderReleases(
+  private renderReleases(
   ): JSX.Element {
     return (
       <>
@@ -147,7 +147,7 @@ class __Content extends React.Component<IContentProps> {
     );
   }
 
-  function renderContent(
+  private renderContent(
   ): JSX.Element {
     return (
       <>
