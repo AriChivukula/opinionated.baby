bc208339945096415ce3468398c094ae228aff7e
Ari Chivukula <ari@chivuku.la>
2018-07-24 13:37:14 -0700

    Update cron.sh

--- shell/cron.sh
+++ shell/cron.sh
@@ -3,5 +3,9 @@ set -e
 export TF_VAR_BRANCH=$TRAVIS_BRANCH
 export TF_VAR_LOCAL_DOMAIN=TEMP.$DOMAIN
 bash shell/build.sh
-terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/PR_*.tfstate" infra/pr || true
-terraform destroy -auto-approve infra/pr || true
+while :
+do
+  export TFSTATE="$(aws s3 ls s3://${TF_VAR_NAME}/PR_*.tfstate --page-size 1 | awk '{print $4}')"
+  echo "no" | terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="${TFSTATE}" infra/pr || true
+  terraform destroy -auto-approve infra/pr || true
+done
