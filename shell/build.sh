6638a49d721c593ba6721dd7fb62308f9db0875c
Ari Chivukula <ari@chivuku.la>
2018-07-23 19:24:04 -0700

    Update build.sh

--- shell/build.sh
+++ shell/build.sh
@@ -4,6 +4,6 @@ yarn gulp build
 yarn install --production=true --ignore-engines
 cp build/3/server/index.js index.js
 cp build/3/server/schema.graphql schema.graphql
-zip -q -r dynamic.zip node_modules index.js schema.graphql
+zip -q -r dynamic.zip node_modules index.js schema.graphql .foia-db.json
 cp -R build/3/website static
 rm -rf index.js schema.graphql
