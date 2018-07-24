33f561f48697755eae5db0d6f62cd212c888161d
Ari Chivukula <ari@chivuku.la>
2018-07-24 10:38:30 -0700

    Update Page.tsx

--- src/website/views/Page.tsx
+++ src/website/views/Page.tsx
@@ -43,18 +43,19 @@ class _Page extends React.Component<IPageProps> {
     if (this.props.data === null) {
       return <LinearProgress determinate={false} />;
     } else if (urlParts.searchParams.has("code")) {
-      // @ts-ignore
       return (
         <>
+          // @ts-ignore
           <TopBar {...this.props} />
           <LinearProgress determinate={false} />
         </>
       );
     } else {
-      // @ts-ignore
       return (
         <>
+          // @ts-ignore
           <TopBar {...this.props} />
+          // @ts-ignore
           <Content {...this.props} />
         </>
       );
