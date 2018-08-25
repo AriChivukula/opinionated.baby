9b7b21bfc1bfa748af7537e918c088bf7adfa012
Travis CI User <travis@example.org>
2018-08-25 00:13:56 +0000

    ACTUALIZE types

--- src/website/views/__generated__/TopBarQuery.graphql.ts
+++ src/website/views/__generated__/TopBarQuery.graphql.ts
@@ -1,9 +1,59 @@
-import { ConcreteFragment } from "relay-runtime";
+/* tslint:disable */
 
+import { ConcreteFragment } from "relay-runtime";
+declare const _TopBarQuery$ref: unique symbol;
+export type TopBarQuery$ref = typeof _TopBarQuery$ref;
 export type TopBarQuery = {
     readonly loginURL: string;
     readonly me: ({
         readonly id: string;
         readonly email: string;
     }) | null;
+    readonly " $refType": TopBarQuery$ref;
 };
+
+
+
+const node: ConcreteFragment = {
+  "kind": "Fragment",
+  "name": "TopBarQuery",
+  "type": "Query",
+  "metadata": null,
+  "argumentDefinitions": [],
+  "selections": [
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "loginURL",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "LinkedField",
+      "alias": null,
+      "name": "me",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "User",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "ScalarField",
+          "alias": null,
+          "name": "id",
+          "args": null,
+          "storageKey": null
+        },
+        {
+          "kind": "ScalarField",
+          "alias": null,
+          "name": "email",
+          "args": null,
+          "storageKey": null
+        }
+      ]
+    }
+  ]
+};
+(node as any).hash = 'd59614096cce9b011824e5cf640fe227';
+export default node;
