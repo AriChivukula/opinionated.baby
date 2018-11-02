e665f103a521729ea3efcb17e91d9b1601f6e06a
Ari Chivukula <ari@chivuku.la>
2018-11-02 08:07:03 -0700

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -2,12 +2,9 @@ set -e
 
 export PATH=$HOME/.local/bin:$PATH
 pip install --user awscli
-wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
-unzip terraform_0.11.8_linux_amd64.zip -d $HOME/.local/bin
-rm terraform_0.11.8_linux_amd64.zip
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-mv cc-test-reporter $HOME/.local/bin/cc-test-reporter
+wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+unzip terraform_0.11.10_linux_amd64.zip -d $HOME/.local/bin
+rm terraform_0.11.10_linux_amd64.zip
 npm install --global --upgrade yarn
 yarn
 yarn picaresque
