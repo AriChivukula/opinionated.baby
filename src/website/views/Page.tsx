85d5f113dab1b77b19329cbeaca45c4d3c6c9365
Ari Chivukula <ari@chivuku.la>
2019-01-30 09:35:21 -0800

    Update Page.tsx

--- src/website/views/Page.tsx
+++ src/website/views/Page.tsx
@@ -11,7 +11,7 @@ import * as React from "react";
 /* BESPOKE START <<imports>> */
 import {
   LinearProgress,
-} from "rmwc/LinearProgress";
+} from "@rmwc/linear-progress";
 import {
   Url,
 } from "url";
