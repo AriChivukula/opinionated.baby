e68b8eca9e80e3d272d00e9ae083c040e6979386
Ari Chivukula <ari@chivuku.la>
2019-02-23 15:16:12 -0800

    Update google.ts

--- src/server/google.ts
+++ src/server/google.ts
@@ -23,17 +23,17 @@ export interface IAccessTokenInfo {
   data: { email?: string | undefined; user_id?: string | undefined; verified_email?: boolean | undefined };
 }
 
-async function genVaultClient(
+async function getVaultClient(
 ): Promise<vault.client> {
   return await vault({
     endpoint: "https://nomoresecrets.chivuku.la/",
     token: process.env.TF_VAR_VAULT_TOKEN,
-  }).init();
+  });
 }
 
 async function genOAuthClient(
 ): Promise<OAuth2Client> {
-  const client = await genVaultClient();
+  const client = getVaultClient();
   const client_id = await client.read("opinionated.baby/TF_VAR_CLIENT_ID");
   const client_secret = await client.read("opinionated.baby/TF_VAR_CLIENT_SECRET");
   return new OAuth2Client(
