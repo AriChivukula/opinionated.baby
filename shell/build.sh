884a554aa38d4ec5bf139c85b1c3cd48b7feb9c2
Ari Chivukula <ari@chivuku.la>
2019-03-03 03:31:24 -0800

    Update build.sh

--- shell/build.sh
+++ shell/build.sh
@@ -1,6 +1,5 @@
 set -e
 
-yarn relay-compiler --src src/ --schema src/server/schema.graphql --language typescript --watchman false
 yarn gulp build
 yarn install --production=true --ignore-engines
 cp build/3/server/index.js index.js
