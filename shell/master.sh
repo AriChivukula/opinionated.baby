95a1e559c85263aa2ebc68d18ab9407c744d3261
Ari Chivukula <ari@chivuku.la>
2019-03-03 03:52:46 -0800

    Update master.sh

--- shell/master.sh
+++ shell/master.sh
@@ -4,7 +4,7 @@ export TF_VAR_BRANCH=master
 export TF_VAR_LOCAL_DOMAIN=$TF_VAR_DOMAIN
 bash shell/test.sh
 bash shell/build.sh
-terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/trigger.tfstate" infra/trigger
+echo "no" | terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/trigger.tfstate" infra/trigger
 terraform apply -auto-approve infra/trigger
-terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/pr
+echo "no" | terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/master.tfstate" infra/pr
 terraform apply -auto-approve infra/pr
