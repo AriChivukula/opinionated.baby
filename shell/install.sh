53e7238667d7729df5d5318ec51291c1044fd93a
Ari Chivukula <ari@chivuku.la>
2019-03-19 19:51:54 -0700

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -2,8 +2,8 @@ set -e
 
 export PATH=$HOME/.local/bin:$PATH
 pip install --user awscli
-wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-unzip terraform_0.11.11_linux_amd64.zip -d $HOME/.local/bin
-rm terraform_0.11.11_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+unzip terraform_0.11.13_linux_amd64.zip -d $HOME/.local/bin
+rm terraform_0.11.13_linux_amd64.zip
 npm install --global --upgrade yarn
 yarn
