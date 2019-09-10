35adf8a0da67f11444932e4395e3a2e72c4cfcde
Ari Chivukula <ari@chivuku.la>
2019-09-10 16:17:20 -0700

    Delete build.sh

--- shell/build.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-yarn gulp build
-yarn install --production=true --ignore-engines
-cp build/3/server/index.js index.js
-cp build/3/server/schema.graphql schema.graphql
-zip -q -r dynamic.zip node_modules index.js schema.graphql .foia-db .foia-db.json
-cp -R build/3/website static
-rm -rf index.js schema.graphql
